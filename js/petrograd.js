ef754be78238f3c5d439561ee2a5734fd7f6f511
Noa Sophie Stoustrup <noasophiestoustrup@Noas-MacBook-Air.local>
2017-02-23 13:25:31 +0100

    add data navn pris og rabatpris

--- js/petrograd.js
+++ js/petrograd.js
@@ -9,7 +9,6 @@ function sidenVises() {
     $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
 
 
-    visProdukt();
 
 }
 
@@ -25,6 +24,11 @@ function visProdukt(produkt) {
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
 
     // indsæt data i klon
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+    var rabatpris = Math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
+    klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
 
     // append klon til .produkt_liste
     document.querySelector(".produktliste").appendChild(klon);
