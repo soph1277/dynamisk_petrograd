9c76aae36eb28007796ae134a0234c4ac7bc2f00
Noa Sophie Stoustrup <noasophiestoustrup@Noas-MacBook-Air.local>
2017-02-23 13:13:23 +0100

    load JSON produktliste and create template clone for each

--- js/petrograd.js
+++ js/petrograd.js
@@ -2,10 +2,24 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
+
+    // læs produktliste
+
+
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
+
     visProdukt();
+
 }
 
-function visProdukt() {
+function visProduktListe(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
+
+function visProdukt(produkt) {
+    console.log(produkt)
 
     // klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
